b5b06488f7a67298eef55549aa861f9ff965ccbb
MissingPotato <potatomissing@gmail.com>
2018-01-11 20:13:32 +0200

    V0.2
    + Added tabs
    + Added map generator
    + Removed the :shit: ty antialiasing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,18 +30,28 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.pictureBox_import = new System.Windows.Forms.PictureBox();
-			this.pictureBox_export = new System.Windows.Forms.PictureBox();
-			this.button_import = new System.Windows.Forms.Button();
-			this.button_fry = new System.Windows.Forms.Button();
-			this.button_export = new System.Windows.Forms.Button();
-			this.label_meme = new System.Windows.Forms.Label();
 			this.label_fryed = new System.Windows.Forms.Label();
+			this.label_meme = new System.Windows.Forms.Label();
+			this.pictureBox_export = new System.Windows.Forms.PictureBox();
+			this.pictureBox_import = new System.Windows.Forms.PictureBox();
+			this.button_DeepFry_import = new System.Windows.Forms.Button();
+			this.button_fry = new System.Windows.Forms.Button();
+			this.button_DeepFry_Export = new System.Windows.Forms.Button();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
 			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage_DeepFry = new System.Windows.Forms.TabPage();
+			this.tabPage_MazeGen = new System.Windows.Forms.TabPage();
+			this.panel_Maze = new System.Windows.Forms.Panel();
+			this.button_MazeGen_Export = new System.Windows.Forms.Button();
+			this.button_MazeGen_Generate = new System.Windows.Forms.Button();
+			this.tabPage_MapGen = new System.Windows.Forms.TabPage();
 			this.panel1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox_import)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox_export)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_import)).BeginInit();
+			this.tabControl1.SuspendLayout();
+			this.tabPage_DeepFry.SuspendLayout();
+			this.tabPage_MazeGen.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// panel1
@@ -52,19 +62,28 @@
 			this.panel1.Controls.Add(this.label_meme);
 			this.panel1.Controls.Add(this.pictureBox_export);
 			this.panel1.Controls.Add(this.pictureBox_import);
-			this.panel1.Location = new System.Drawing.Point(155, 12);
+			this.panel1.Location = new System.Drawing.Point(399, 12);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(311, 641);
+			this.panel1.Size = new System.Drawing.Size(311, 617);
 			this.panel1.TabIndex = 1;
 			// 
-			// pictureBox_import
+			// label_fryed
 			// 
-			this.pictureBox_import.Location = new System.Drawing.Point(32, 36);
-			this.pictureBox_import.Name = "pictureBox_import";
-			this.pictureBox_import.Size = new System.Drawing.Size(256, 256);
-			this.pictureBox_import.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox_import.TabIndex = 0;
-			this.pictureBox_import.TabStop = false;
+			this.label_fryed.AutoSize = true;
+			this.label_fryed.Location = new System.Drawing.Point(128, 344);
+			this.label_fryed.Name = "label_fryed";
+			this.label_fryed.Size = new System.Drawing.Size(37, 13);
+			this.label_fryed.TabIndex = 3;
+			this.label_fryed.Text = "Export";
+			// 
+			// label_meme
+			// 
+			this.label_meme.AutoSize = true;
+			this.label_meme.Location = new System.Drawing.Point(143, 13);
+			this.label_meme.Name = "label_meme";
+			this.label_meme.Size = new System.Drawing.Size(36, 13);
+			this.label_meme.TabIndex = 2;
+			this.label_meme.Text = "Import";
 			// 
 			// pictureBox_export
 			// 
@@ -75,74 +94,143 @@
 			this.pictureBox_export.TabIndex = 1;
 			this.pictureBox_export.TabStop = false;
 			// 
-			// button_import
+			// pictureBox_import
 			// 
-			this.button_import.Location = new System.Drawing.Point(12, 12);
-			this.button_import.Name = "button_import";
-			this.button_import.Size = new System.Drawing.Size(123, 26);
-			this.button_import.TabIndex = 2;
-			this.button_import.Text = "Import Meme";
-			this.button_import.UseVisualStyleBackColor = true;
-			this.button_import.Click += new System.EventHandler(this.button_import_Click);
+			this.pictureBox_import.Location = new System.Drawing.Point(32, 36);
+			this.pictureBox_import.Name = "pictureBox_import";
+			this.pictureBox_import.Size = new System.Drawing.Size(256, 256);
+			this.pictureBox_import.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox_import.TabIndex = 0;
+			this.pictureBox_import.TabStop = false;
+			// 
+			// button_DeepFry_import
+			// 
+			this.button_DeepFry_import.Location = new System.Drawing.Point(6, 20);
+			this.button_DeepFry_import.Name = "button_DeepFry_import";
+			this.button_DeepFry_import.Size = new System.Drawing.Size(123, 26);
+			this.button_DeepFry_import.TabIndex = 2;
+			this.button_DeepFry_import.Text = "Import";
+			this.button_DeepFry_import.UseVisualStyleBackColor = true;
+			this.button_DeepFry_import.Click += new System.EventHandler(this.button_import_Click);
 			// 
 			// button_fry
 			// 
-			this.button_fry.Location = new System.Drawing.Point(12, 44);
+			this.button_fry.Location = new System.Drawing.Point(6, 52);
 			this.button_fry.Name = "button_fry";
 			this.button_fry.Size = new System.Drawing.Size(123, 26);
 			this.button_fry.TabIndex = 3;
-			this.button_fry.Text = "Deep Fry";
+			this.button_fry.Text = "Execute";
 			this.button_fry.UseVisualStyleBackColor = true;
 			this.button_fry.Click += new System.EventHandler(this.button_fry_Click);
 			// 
-			// button_export
+			// button_DeepFry_Export
 			// 
-			this.button_export.Location = new System.Drawing.Point(12, 76);
-			this.button_export.Name = "button_export";
-			this.button_export.Size = new System.Drawing.Size(123, 26);
-			this.button_export.TabIndex = 4;
-			this.button_export.Text = "Export Meme";
-			this.button_export.UseVisualStyleBackColor = true;
-			this.button_export.Click += new System.EventHandler(this.button_export_Click);
-			// 
-			// label_meme
-			// 
-			this.label_meme.AutoSize = true;
-			this.label_meme.Location = new System.Drawing.Point(143, 13);
-			this.label_meme.Name = "label_meme";
-			this.label_meme.Size = new System.Drawing.Size(36, 13);
-			this.label_meme.TabIndex = 2;
-			this.label_meme.Text = "Meme";
-			// 
-			// label_fryed
-			// 
-			this.label_fryed.AutoSize = true;
-			this.label_fryed.Location = new System.Drawing.Point(128, 344);
-			this.label_fryed.Name = "label_fryed";
-			this.label_fryed.Size = new System.Drawing.Size(65, 13);
-			this.label_fryed.TabIndex = 3;
-			this.label_fryed.Text = "Fryed Meme";
+			this.button_DeepFry_Export.Location = new System.Drawing.Point(135, 20);
+			this.button_DeepFry_Export.Name = "button_DeepFry_Export";
+			this.button_DeepFry_Export.Size = new System.Drawing.Size(123, 26);
+			this.button_DeepFry_Export.TabIndex = 4;
+			this.button_DeepFry_Export.Text = "Export";
+			this.button_DeepFry_Export.UseVisualStyleBackColor = true;
+			this.button_DeepFry_Export.Click += new System.EventHandler(this.button_export_Click);
 			// 
 			// openFileDialog1
 			// 
 			this.openFileDialog1.FileName = "openFileDialog1";
 			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage_DeepFry);
+			this.tabControl1.Controls.Add(this.tabPage_MazeGen);
+			this.tabControl1.Controls.Add(this.tabPage_MapGen);
+			this.tabControl1.Location = new System.Drawing.Point(12, 12);
+			this.tabControl1.Multiline = true;
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(381, 616);
+			this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+			this.tabControl1.TabIndex = 5;
+			// 
+			// tabPage_DeepFry
+			// 
+			this.tabPage_DeepFry.Controls.Add(this.button_fry);
+			this.tabPage_DeepFry.Controls.Add(this.button_DeepFry_Export);
+			this.tabPage_DeepFry.Controls.Add(this.button_DeepFry_import);
+			this.tabPage_DeepFry.Location = new System.Drawing.Point(4, 22);
+			this.tabPage_DeepFry.Name = "tabPage_DeepFry";
+			this.tabPage_DeepFry.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage_DeepFry.Size = new System.Drawing.Size(373, 590);
+			this.tabPage_DeepFry.TabIndex = 0;
+			this.tabPage_DeepFry.Text = "DeepFry";
+			this.tabPage_DeepFry.UseVisualStyleBackColor = true;
+			// 
+			// tabPage_MazeGen
+			// 
+			this.tabPage_MazeGen.Controls.Add(this.panel_Maze);
+			this.tabPage_MazeGen.Controls.Add(this.button_MazeGen_Export);
+			this.tabPage_MazeGen.Controls.Add(this.button_MazeGen_Generate);
+			this.tabPage_MazeGen.Location = new System.Drawing.Point(4, 22);
+			this.tabPage_MazeGen.Name = "tabPage_MazeGen";
+			this.tabPage_MazeGen.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage_MazeGen.Size = new System.Drawing.Size(373, 590);
+			this.tabPage_MazeGen.TabIndex = 1;
+			this.tabPage_MazeGen.Text = "MazeGen";
+			this.tabPage_MazeGen.UseVisualStyleBackColor = true;
+			// 
+			// panel_Maze
+			// 
+			this.panel_Maze.Location = new System.Drawing.Point(16, 43);
+			this.panel_Maze.Name = "panel_Maze";
+			this.panel_Maze.Size = new System.Drawing.Size(351, 334);
+			this.panel_Maze.TabIndex = 2;
+			this.panel_Maze.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Maze_Paint);
+			// 
+			// button_MazeGen_Export
+			// 
+			this.button_MazeGen_Export.Location = new System.Drawing.Point(255, 14);
+			this.button_MazeGen_Export.Name = "button_MazeGen_Export";
+			this.button_MazeGen_Export.Size = new System.Drawing.Size(112, 23);
+			this.button_MazeGen_Export.TabIndex = 1;
+			this.button_MazeGen_Export.Text = "Export Maze";
+			this.button_MazeGen_Export.UseVisualStyleBackColor = true;
+			// 
+			// button_MazeGen_Generate
+			// 
+			this.button_MazeGen_Generate.Location = new System.Drawing.Point(16, 14);
+			this.button_MazeGen_Generate.Name = "button_MazeGen_Generate";
+			this.button_MazeGen_Generate.Size = new System.Drawing.Size(112, 23);
+			this.button_MazeGen_Generate.TabIndex = 0;
+			this.button_MazeGen_Generate.Text = "Generate Maze";
+			this.button_MazeGen_Generate.UseVisualStyleBackColor = true;
+			this.button_MazeGen_Generate.Click += new System.EventHandler(this.button_MazeGen_Generate_Click);
+			// 
+			// tabPage_MapGen
+			// 
+			this.tabPage_MapGen.Location = new System.Drawing.Point(4, 22);
+			this.tabPage_MapGen.Name = "tabPage_MapGen";
+			this.tabPage_MapGen.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage_MapGen.Size = new System.Drawing.Size(373, 590);
+			this.tabPage_MapGen.TabIndex = 2;
+			this.tabPage_MapGen.Text = "MapGen";
+			this.tabPage_MapGen.UseVisualStyleBackColor = true;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 665);
-			this.Controls.Add(this.button_export);
-			this.Controls.Add(this.button_fry);
-			this.Controls.Add(this.button_import);
+			this.ClientSize = new System.Drawing.Size(728, 641);
+			this.Controls.Add(this.tabControl1);
 			this.Controls.Add(this.panel1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MinimumSize = new System.Drawing.Size(744, 680);
 			this.Name = "Form1";
 			this.Text = "Meme Deep Fryer";
 			this.panel1.ResumeLayout(false);
 			this.panel1.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox_import)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox_export)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox_import)).EndInit();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage_DeepFry.ResumeLayout(false);
+			this.tabPage_MazeGen.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -152,13 +240,20 @@
 		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.PictureBox pictureBox_export;
 		private System.Windows.Forms.PictureBox pictureBox_import;
-		private System.Windows.Forms.Button button_import;
+		private System.Windows.Forms.Button button_DeepFry_import;
 		private System.Windows.Forms.Button button_fry;
 		private System.Windows.Forms.Label label_fryed;
 		private System.Windows.Forms.Label label_meme;
-		private System.Windows.Forms.Button button_export;
+		private System.Windows.Forms.Button button_DeepFry_Export;
 		private System.Windows.Forms.OpenFileDialog openFileDialog1;
 		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage_DeepFry;
+		private System.Windows.Forms.TabPage tabPage_MazeGen;
+		private System.Windows.Forms.TabPage tabPage_MapGen;
+		private System.Windows.Forms.Button button_MazeGen_Export;
+		private System.Windows.Forms.Button button_MazeGen_Generate;
+		private System.Windows.Forms.Panel panel_Maze;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace MemeDeepFrier
 {
+
+	
+
 	public partial class Form1 : Form
 	{
+		
 
 		public static Bitmap meme;
 		public static Bitmap friedMeme;
@@ -19,6 +24,7 @@ namespace MemeDeepFrier
 		public Form1()
 		{
 			InitializeComponent();
+			
 		}
 
 		private void button_import_Click(object sender, EventArgs e)
@@ -94,6 +100,28 @@ namespace MemeDeepFrier
 				friedMeme.Save(saveFileDialog1.FileName);
 			}
 		}
+		
+		public Bitmap ResizeBitmap(Bitmap bitInput, int width, int height)
+		{
+
+			Graphics mygraphics = Graphics.FromImage(bitInput);
+			mygraphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+			Bitmap outPut = new Bitmap(width, height, mygraphics);
+
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					if ( x % 2 == 0)
+						outPut.SetPixel(x, y, Color.Red);
+					else
+						outPut.SetPixel(x, y, Color.Blue);
+				}
+			}
+
+			return outPut;
+		
+		}
 
 		/// <summary>
 		/// Previews the fried meme
@@ -114,11 +142,61 @@ namespace MemeDeepFrier
 			{
 
 				meme = new Bitmap(openFileDialog1.FileName);
+				
 				pictureBox_import.Image = meme;
+
 			}
+		}
+
+
+
+
+		#endregion
+
+		#region Maze
+
+		public Graphics g;
+
+
+		private void button_MazeGen_Generate_Click(object sender, EventArgs e)
+		{
+
+			Bitmap test = new Bitmap(20, 20);
+
+			for (int i = 1; i < 20; i++)
+			{
+				for (int j = 1; j < 20; j++)
+				{
+					
+					if ((i + j) % 2 == 0)
+						test.SetPixel(i, j, Color.Red);
+					else
+						test.SetPixel(i, j, Color.Blue);
+
+					test.SetPixel(1, 1, Color.Black);
+					test.SetPixel(1, 19, Color.Black);
+					test.SetPixel(19, 1, Color.Black);
+					test.SetPixel(19, 19, Color.Black);
+
+
+				}
+			}
+
+			g.DrawImage(test, panel_Maze.DisplayRectangle);
+		}
+
+		private void panel_Maze_Paint(object sender, EventArgs e)
+		{
+			g = panel_Maze.CreateGraphics();
+
+			g.InterpolationMode = InterpolationMode.NearestNeighbor;
+			g.FillRectangle(new SolidBrush(Color.White), panel_Maze.DisplayRectangle);
+
 		}
 
 		#endregion
 
 	}
+
+
 }
